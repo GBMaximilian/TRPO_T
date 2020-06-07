3410135b6881983e7835f3bceaf128132295fb76
GBMaximilian <GBMaximilian@users.noreply.github.com>
2020-06-07 17:16:22 +0300

    23

--- TRPO_Kurs/TRPO_Kurs/Models/Equipment.cs
+++ TRPO_Kurs/TRPO_Kurs/Models/Equipment.cs
@@ -12,7 +12,7 @@ namespace TRPO_Kurs.Models
         public string Producer { get; set; }
         public int Price { get; set; }
         public int CategoryId { get; set; }
-        public string Image { get; set; }
+        public string Image { get; set; } 
         public string Description { get; set; }
         public string Text { get; set; }
     }
